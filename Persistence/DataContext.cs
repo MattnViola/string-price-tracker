5afa2bc666057e9925697731a4ce0f8973bb06c0
Matt Norman <mrnorman.sev@gmail.com>
2023-11-01 00:23:41 -0400

    Updates Dbcontext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,12 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Vendor> Vendors { get; set; }
+
+        public DbSet<Price> Prices { get; set; }
+
          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=string-price-tracker;Username=postgres;Password=test");
 
